e946b4f56248b2e3cc127ad6e65afb9025cc80ec
muriloeduardo <muriloeduardoooooo@gmail.com>
2023-10-23 09:45:09 -0300

    Add trilling comma

--- services/mongoService.js
+++ services/mongoService.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose')
-const { MONGO_URL } = require('../env')
+const mongoose = require('mongoose');
+const { MONGO_URL } = require('../env');
 
 (async () => {
   try {
