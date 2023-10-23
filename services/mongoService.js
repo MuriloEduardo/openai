bc41bfea4c472b037994fbece87f8f30029ab3b1
muriloeduardo <muriloeduardoooooo@gmail.com>
2023-10-23 09:43:38 -0300

    Change find return

--- services/mongoService.js
+++ services/mongoService.js
@@ -1,25 +1,26 @@
 const mongoose = require('mongoose')
 const { MONGO_URL } = require('../env')
 
-  (async () => {
-    try {
-      await mongoose.connect(MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
+(async () => {
+  try {
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
 
-      console.log('Conectado ao MongoDB')
-    } catch (error) {
-      console.error('Erro ao conectar ao MongoDB:', error)
-    }
-  })()
+    console.log('Conectado ao MongoDB')
+  } catch (error) {
+    console.error('Erro ao conectar ao MongoDB:', error)
+  }
+})()
 
 const insert = (model, data) => {
   const newDoc = new model(data)
   return newDoc.save()
 }
 
-const find = (model, filters = {}) =>
-  model.find(filters).exec()
+const find = (model, filters = {}) => {
+  return model.find(filters).exec()
+}
 
 module.exports = { insert, find }
